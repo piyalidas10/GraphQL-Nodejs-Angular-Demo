ede5fb7bf9a00fcaec5bf2d5170c443962c5e7fd
piyalidas10 <piyalidas.it@gmail.com>
2023-04-30 20:44:27 +0530

    file change

---
+++ config.js
@@ -0,0 +1,4 @@
+const MONGO_URL = 'mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority';
+const JWT_SECRET = 'sdffsdfsdf';
+
+module.exports = {MONGO_URL, JWT_SECRET};
